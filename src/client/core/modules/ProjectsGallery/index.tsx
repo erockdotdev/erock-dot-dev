bb10e11336133c63b085c74922f14a21d2d2e8b6
Eric Sanchez <eric.sanchez@equinox.com>
2020-05-08 22:40:50 -0400

    add carousel to gallery.

--- src/client/core/modules/ProjectsGallery/index.tsx
+++ src/client/core/modules/ProjectsGallery/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { chunk } from 'lodash';
 import { Project } from '@custom-types/index';
 import ProjectCard from '@components/ProjectCard';
+import Slider from 'react-slick';
 import './projects-gallery.scss';
 
 type Props = {
@@ -26,26 +27,57 @@ function renderProjects(projects: Project[]) {
   );
 }
 
-function renderGroup(projects: Project[]): React.ReactNode {
-  const groupProjects = chunk(projects, 2);
-  return groupProjects.map((group, index) => {
-    const key = `group-${index}`;
-    return (
-      <div key={key} className="projects-gallery__container__inner__group">
-        {renderProjects(group)}
-      </div>
-    );
-  });
+// function renderGroup(projects: Project[]): React.ReactNode {
+//   const groupProjects = chunk(projects, 2);
+//   return groupProjects.map((group, index) => {
+//     const key = `group-${index}`;
+//     return (
+//       <div key={key} className="projects-gallery__container__inner__group">
+//         {renderProjects(group)}
+//       </div>
+//     );
+//   });
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'red' }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'green' }}
+      onClick={onClick}
+    />
+  );
 }
 
 const ProjectsGallery: React.FC<Props> = ({ fields }) => {
   const { sectionTitle, projects } = fields;
+  const settings = {
+    speed: 500,
+    slidesToShow: 3.5,
+    slidesToScroll: 3,
+    dots: false,
+    infinite: false,
+    className: 'inner-slider-div',
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
   return (
     <section className="projects-gallery__container">
       I am a {sectionTitle}!
       <div className="projects-gallery__container__inner">
-        {renderGroup(projects)}
+        <Slider {...settings}>{renderProjects(projects)}</Slider>
       </div>
+      {/* seperate previous and next buttons https://react-slick.neostack.com/docs/example/previous-next-methods */}
     </section>
   );
 };
