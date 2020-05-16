1962adeacb1086942d052beb54ee5274bbf5e83a
Eric Sanchez <eric.sanchez@equinox.com>
2020-05-16 02:41:39 -0400

    add previous and next buttons to slider.

--- src/client/core/modules/ProjectsGallery/index.tsx
+++ src/client/core/modules/ProjectsGallery/index.tsx
@@ -3,6 +3,7 @@ import { Project } from '@custom-types/index';
 import ProjectCard from '@components/ProjectCard';
 import Slider from 'react-slick';
 import './projects-gallery.scss';
+import classNames from 'classnames';
 
 type Props = {
   fields: {
@@ -26,48 +27,85 @@ function renderProjects(projects: Project[]) {
   );
 }
 
-function SampleNextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: 'block', background: 'red' }}
-      onClick={onClick}
-    />
-  );
-}
-
-function SamplePrevArrow(props) {
-  const { className, style, onClick } = props;
-  console.log('props', props);
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: 'block', background: 'green' }}
-      onClick={onClick}
-    />
-  );
-}
-
 const ProjectsGallery: React.FC<Props> = ({ fields }) => {
   const { sectionTitle, projects } = fields;
+
   const settings = {
     speed: 500,
     slidesToShow: 3.5,
     slidesToScroll: 3,
     dots: false,
     infinite: false,
-    className: 'inner-slider-div',
+    draggable: false,
     nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1,
+          draggable: true,
+          nextArrow: <div />,
+          prevArrow: <div />
+        }
+      }
+    ]
   };
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    const isDisabled = className.includes('slick-disabled');
+    const nextArrowClassNames = classNames(
+      className,
+      'projects-gallery__container__next-arrow',
+      {
+        'projects-gallery__container__hide-arrow': isDisabled
+      }
+    );
+    return (
+      <div
+        className={nextArrowClassNames}
+        style={{ ...style, background: 'black' }}
+        onClick={onClick}
+        onKeyDown={onClick}
+        role="button"
+        aria-label="Slider Next Button"
+        tabIndex={0}
+      />
+    );
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    const isDisabled = className.includes('slick-disabled');
+    const prevArrowClassNames = classNames(
+      className,
+      'projects-gallery__container__prev-arrow',
+      {
+        'projects-gallery__container__hide-arrow': isDisabled
+      }
+    );
+
+    return (
+      <div
+        className={prevArrowClassNames}
+        style={{ ...style, background: 'black' }}
+        onClick={onClick}
+        onKeyDown={onClick}
+        role="button"
+        aria-label="Slider Previous Button"
+        tabIndex={0}
+      />
+    );
+  }
+
   return (
     <section className="projects-gallery__container">
       <span className="projects-gallery__container__title">{sectionTitle}</span>
       <div className="projects-gallery__container__inner">
         <Slider {...settings}>{renderProjects(projects)}</Slider>
       </div>
-      {/* seperate previous and next buttons https://react-slick.neostack.com/docs/example/previous-next-methods */}
     </section>
   );
 };
